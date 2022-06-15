01aa7ce130ba07b15bf431112d078cf4c4892b42
Charlie <ware.charlie914@gmail.com>
2022-06-14 20:05:27 -0700

    Added total price counter

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,29 @@
-//Quantity Button
+//Information Update Buttons 
 
 function quantityUpdate() {
     let qNum = document.querySelector('.total-quantity')
     qNum.innerHTML = 'Quantity: '+quantity
 }
 
+function priceUpdate() {
+    let price = document.querySelector('.total-price')
+    price.innerHTML = 'Total Price available at checkout is: $ '+totalPrice
+}
+
+//Variables
 let quantity = 1
+let price = 15.00
 let addOne = document.getElementById("quantity-up")
 let subOne = document.getElementById("quantity-down")
 let remove = document.querySelector(".remove")
 
+//Quantity and Remove Buttons
 addOne.addEventListener('click', function() {
     quantity = quantity + 1
     console.log(quantity)
+    totalPrice = price * quantity
+    priceUpdate()
+   
     quantityUpdate()
 })
 
@@ -24,15 +35,14 @@ subOne.addEventListener('click', function() {
     else{
         quantity = quantity + 0
     }
+    totalPrice = price * quantity
+    priceUpdate()
     quantityUpdate()
 })
 
 remove.addEventListener('click', function(){
     quantity = 0
+    totalPrice = price * quantity
     quantityUpdate()
+    priceUpdate()
 })
-
-
-
-
-
